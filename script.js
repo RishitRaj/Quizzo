6f028884eabfb7a136acde00aaa94160c859b775
RishitRaj <140539552+RishitRaj@users.noreply.github.com>
2025-07-13 12:56:27 +0530

    Final changes + images uploaded

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let form = document.querySelector("#quizform");
-if (form){
-    form.addEventListener("submit",(event)=>{
-        event.preventDefault();
-        let topic = document.querySelector('input[name="topic"]:checked');
+document.addEventListener("DOMContentLoaded", () => {
+    let form = document.querySelector("#quizform");
+    if (form) {
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            let topic = document.querySelector('input[name="topic"]:checked');
             if (topic) {
                 let page = "";
                 switch (topic.value) {
@@ -23,14 +24,49 @@ if (form){
             } else {
                 alert("Please select a topic before starting the quiz.");
             }
-    })
-}
- let questions = document.querySelectorAll(".question");
- let nextButtons = document.querySelectorAll(".next_button");
+        });
+    }
 
-nextButtons.forEach((btn, index) => {
-      btn.addEventListener("click", () => {
-        questions[index].classList.remove("active");
-        questions[index + 1].classList.add("active");
-      });
+    let questions = document.querySelectorAll(".question");
+    let nextButtons = document.querySelectorAll(".next_button");
+    let resultbox = document.querySelector(".result");
+
+    nextButtons.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            questions[index].classList.remove("active");
+            if (questions[index + 1]) {
+                questions[index + 1].classList.add("active");
+            }
+        });
     });
+
+    let score=0;
+    let yourscore= document.querySelector("#score");
+    let submit = document.querySelector("#submitquiz");
+    if (submit) {
+        submit.addEventListener("click", (event) => {
+            event.preventDefault();
+            questions.forEach((q) => {
+                const correct= q.dataset.answer;
+                const selected = q.querySelector("input[type='radio']:checked");
+
+            if (selected && selected.value === correct) {
+                score++;
+            }
+            })
+            if (resultbox) {
+                resultbox.style.display = "block";
+                yourscore.innerText= `Your Score: ${score}`;
+            }
+        });
+    }
+    let options = document.querySelectorAll("input[type='radio']");
+    let again= document.querySelector("#play_again");
+    again.addEventListener("click",()=>{
+        score=0;
+        questions[9].classList.remove("active");
+        questions[0].classList.add("active");
+        resultbox.style.display="none";
+        options.forEach(opt => opt.checked = false);
+    })
+});
